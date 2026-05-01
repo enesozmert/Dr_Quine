080be5406623b761f3caca288532a4d0fc214e16
Enes Özmert <ozmerte@gmail.com>
2026-05-02 02:01:10 +0300

    Remove obsolete ASM and C quine files for Colleen, Grace, and Sully

--- output/C/Colleen.c
+++
@@ -1,5 +0,0 @@
-/*outside*/
-#include<stdio.h>
-void f(void){}
-char*s="/*outside*/%c#include<stdio.h>%cvoid f(void){}%cchar*s=%c%s%c;%cint main(void){f();/*inside*/printf(s,10,10,10,34,s,34,10,10);return 0;}%c";
-int main(void){f();/*inside*/printf(s,10,10,10,34,s,34,10,10);return 0;}
